6e2862bcdd18dacc528749e07db06a40d6f983b0
naree-shin <63437109+naree-shin@users.noreply.github.com>
2024-01-10 17:01:19 +0900

    react-responsive ing...

---
+++ src/app/(news)/layout.js
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }) {
+    return (
+        <>
+            <p>news group</p>
+            {children}
+        </>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 import Image from "next/image";
 import img01 from "/public/images/testimg.jpg";
+import { useMediaQuery } from "react-responsive";
 
 export default function Home() {
+    // const [isPc, setIsPc] = useState(false);
+    const isPC = useMediaQuery({
+        query: "(min-width: 1200px)",
+    });
+    const isTablet = useMediaQuery({
+        query: "(min-width: 798px)",
+    });
+    const isMo = useMediaQuery({
+        query: "(max-width: 797px)",
+    });
     return (
         <>
             <p>home</p>
+            {isPC && <p>PC</p>}
+            {isTablet && <p>Tablet</p>}
+            {isMo && <p>Mo</p>}
             <Image
                 src={img01}
                 width={500}
